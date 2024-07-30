25f5efc7bfa4251acd3813b41ca475e17652d7d4
Rodolfo Rohatan Bassil <bassilmatteo22@gmail.com>
2024-07-29 19:25:01 -0500

    incorporación del endpoint logout a la carpeta restify y publicación de controladores

--- app/Http/Controllers/Restify/Auth/LogoutController.php
+++ app/Http/Controllers/Restify/Auth/LogoutController.php
@@ -1,18 +1,18 @@
 <?php
-namespace App\Http\Controllers\Api;
-
-use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Requests\RegisterRequest;
-use Illuminate\Support\Facades\Hash;
+namespace App\Http\Controllers\Restify\Auth;
 
 use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Hash;
+
+
 
 
 class logoutController extends Controller
 {
     
-    public function logout(Request $request)
+    public function __invoke(Request $request)
     {
 
        // Elimina todos los tokens del usuario autenticado
--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Restify\Auth\LogoutController;
 
-use App\Http\Controllers\Api\logoutController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -13,4 +13,40 @@ Route::get('/user', function (Request $request) {
 //Route::restifyAuth(actions: ['login', 'register']);
 Route::restifyAuth();
 
-Route::middleware('auth:sanctum')->delete('auth/logout', [LogoutController::class, 'logout']);
+//Route::middleware('auth:sanctum')->delete('auth/logout', [LogoutController::class, 'logout']);
+
+Route::delete('LogoutController', \App\Http\Controllers\Restify\Auth\LogoutController::class)
+    ->middleware('auth:sanctum')
+    ->name('restify.LogoutController');
+
+Route::post('login', \App\Http\Controllers\Restify\Auth\LoginController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.login');
+
+Route::post('register', \App\Http\Controllers\Restify\Auth\RegisterController::class)
+    ->name('restify.register');
+
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+
+Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.forgotPassword');
+
+Route::post('resetPassword', \App\Http\Controllers\Restify\Auth\ResetPasswordController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.resetPassword');
+
+Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.verify');
+
+Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
+    ->middleware('throttle:6,1')
+    ->name('restify.verify');
+
