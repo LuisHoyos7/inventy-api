a7bb6ca2d793e92bcb12e85d3edc6365ff1329e4
Rodolfo Rohatan Bassil <bassilmatteo22@gmail.com>
2024-08-19 11:03:35 -0500

    avance del crud branches

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Restify\Auth\LogoutController;
-
+use App\Restify\BrancheRepository;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Restify::repositories([
+    BrancheRepository::class,
+]);
 
 //Route::restifyAuth(actions: ['login', 'register']);
 Route::restifyAuth();
@@ -19,34 +22,4 @@ Route::delete('LogoutController', \App\Http\Controllers\Restify\Auth\LogoutContr
     ->middleware('auth:sanctum')
     ->name('restify.LogoutController');
 
-Route::post('login', \App\Http\Controllers\Restify\Auth\LoginController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.login');
-
-Route::post('register', \App\Http\Controllers\Restify\Auth\RegisterController::class)
-    ->name('restify.register');
-
-Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.forgotPassword');
-
-Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.forgotPassword');
-
-Route::post('forgotPassword', \App\Http\Controllers\Restify\Auth\ForgotPasswordController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.forgotPassword');
-
-Route::post('resetPassword', \App\Http\Controllers\Restify\Auth\ResetPasswordController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.resetPassword');
-
-Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.verify');
-
-Route::post('verify/{id}/{hash}', \App\Http\Controllers\Restify\Auth\VerifyController::class)
-    ->middleware('throttle:6,1')
-    ->name('restify.verify');
 
