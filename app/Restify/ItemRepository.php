3b730c4cb2fcd2184394a6fb7ebfd61329d3cc2b
Deiver Julio <deiver-jc@live.com>
2024-08-30 06:57:25 -0500

    feat: agrega relacion a nivel de restify entre Item & Category

--- app/Restify/ItemRepository.php
+++ app/Restify/ItemRepository.php
@@ -4,11 +4,9 @@ namespace App\Restify;
 
 use App\Models\Category;
 use App\Models\Item;
+use Binaryk\LaravelRestify\Fields\BelongsTo;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Binaryk\LaravelRestify\Repositories\Repository;
-use Binaryk\LaravelRestify\Fields\BelongsTo;
-use Binaryk\LaravelRestify\Fields\HasMany;
-use Illuminate\Http\JsonResponse;
 use Illuminate\Validation\Rule;
 
 class ItemRepository extends Repository
@@ -23,29 +21,13 @@ class ItemRepository extends Repository
             field('barcode')->rules('required', 'min:3', 'max:100'),
             field('initial_cost')->rules('required', 'numeric'),
             field('category_id')->rules('required', Rule::exists(Category::class, 'id')),
-
-            // // Relación con ItemType
-            // BelongsTo::make('Item Type', 'itemType', ItemTypeRepository::class),
-
-            // // Relación con Category
-            // BelongsTo::make('Category', 'category', CategoryRepository::class),
-
-            // // Relación con PriceList
-            // HasMany::make('Price Lists', 'priceLists', PriceListRepository::class),
-
-            // // Relación con InvoiceDetail
-            // HasMany::make('Invoice Details', 'invoiceDetails', InvoiceDetailRepository::class),
         ];
     }
 
-    // public function store(RestifyRequest $request)
-    // {
-    //     // Personaliza la lógica de almacenamiento aquí
-    //     $item = Item::create($request->all());
-
-    //     //creamos la lista de precio por defecto
-    //     PreciList::create([]);
-
-    //     return rest($item)->indexMeta([]);
-    // }
+    public static function related(): array
+    {
+        return [
+            'category' => BelongsTo::make('category'),
+        ];
+    }
 }
