2735bad55455d94c1a450d9aeb90f9d487dd025d
Deiver Julio <deiver-jc@live.com>
2024-08-30 06:56:33 -0500

    feat: actualiza ItemPolicy

--- app/Policies/ItemPolicy.php
+++ app/Policies/ItemPolicy.php
@@ -2,6 +2,7 @@
 
 namespace App\Policies;
 
+use App\Models\Item;
 use Illuminate\Auth\Access\HandlesAuthorization;
 
 use App\Models\User;
@@ -15,7 +16,37 @@ class ItemPolicy
         return true;
     }
 
+    public function show(User $user = null, Item $model): bool
+    {
+        return true;
+    }
+
     public function store(User $user): bool
+    {
+        return true;
+    }
+
+    public function storeBulk(User $user): bool
+    {
+        return false;
+    }
+
+    public function update(User $user, Item $model): bool
+    {
+        return true;
+    }
+
+    public function updateBulk(User $user, Item $model): bool
+    {
+        return false;
+    }
+
+    public function deleteBulk(User $user, Item $model): bool
+    {
+        return false;
+    }
+
+    public function delete(User $user, Item $model): bool
     {
         return true;
     }
