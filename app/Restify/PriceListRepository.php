33d42528aadb0d55a7a69e237295742ea24abd0b
Deiver Julio <deiver-jc@live.com>
2024-09-22 21:43:03 -0500

    feat(price lists): update PriceListRepository add relation with company

--- app/Restify/PriceListRepository.php
+++ app/Restify/PriceListRepository.php
@@ -2,22 +2,26 @@
 
 namespace App\Restify;
 
-use App\Models\Company;
 use App\Models\PriceList;
+use Binaryk\LaravelRestify\Fields\BelongsTo;
 use Binaryk\LaravelRestify\Http\Requests\RestifyRequest;
 use Binaryk\LaravelRestify\Repositories\Repository;
-use Binaryk\LaravelRestify\Fields\BelongsTo;
-use Illuminate\Validation\Rule;
 
 class PriceListRepository extends Repository
 {
     public static string $model = PriceList::class;
-    public static string $uriKey = 'pricelist';
 
     public function fields(RestifyRequest $request): array
     {
         return [
             field('name')->rules('required', 'min:3', 'max:255'),
+        ];
+    }
+
+    public static function related(): array
+    {
+        return [
+            'company' => BelongsTo::make('company'),
         ];
     }
 }
