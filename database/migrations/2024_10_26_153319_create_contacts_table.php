bb14c6398542dd650e9c6080bfb75341bc65a1d6
Rodolfo Rohatan <bassilmatteo22@gmail.com>
2024-10-26 13:11:49 -0500

    agrega migracion para el modelo contact

---
+++ database/migrations/2024_10_26_153319_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Doctrine\DBAL\Schema\Column;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('identification', 20);
+            $table->string('fullname');
+            $table->string('email');
+            $table->boolean('is_customer')->default(false);
+            $table->boolean('is_supplier')->default(false);
+            $table->foreignId('company_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
