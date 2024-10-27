ebae1a99e106d8d04b0a8a09081ebeaf04dec474
Deiver Julio <deiver-jc@live.com>
2024-10-27 11:45:22 -0500

    feat(transactions): add status field to Transaction model

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
+use App\Enums\TransactionStatus;
 use App\Enums\TransactionsType;
 use App\Traits\HasCompany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Transaction extends Model
 {
@@ -16,13 +18,20 @@ class Transaction extends Model
         'type',
         'date',
         'total',
-        'company_id'
+        'status',
+        'company_id',
     ];
 
     protected function casts(): array
     {
         return [
             'type' => TransactionsType::class,
+            'status' => TransactionStatus::class,
         ];
+    }
+
+    public function contact(): BelongsTo
+    {
+        return $this->belongsTo(Contact::class);
     }
 }
