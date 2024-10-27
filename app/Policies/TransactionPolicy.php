e9513e83442f15f98a3e6f8407ab75cde9c33686
Deiver Julio <deiver-jc@live.com>
2024-10-27 12:37:39 -0500

    feat(transactions): update TransactionPolicy

--- app/Policies/TransactionPolicy.php
+++ app/Policies/TransactionPolicy.php
@@ -2,9 +2,11 @@
 
 namespace App\Policies;
 
+use App\Models\Item;
 use App\Models\Transaction;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
+use Illuminate\Support\Collection;
 
 class TransactionPolicy
 {
@@ -48,5 +50,20 @@ class TransactionPolicy
     public function delete(User $user, Transaction $model): bool
     {
         return false;
+    }
+
+    public function attachItems(User $authenticatedUser, Transaction $model, Item $item): bool
+    {
+        return true;
+    }
+
+    public function detachItems(User $authenticatedUser, Transaction $model, Item $item): bool
+    {
+        return true;
+    }
+
+    public function syncItems(User $authenticatedUser, Transaction $model, Collection $keys): bool
+    {
+        return true;
     }
 }
